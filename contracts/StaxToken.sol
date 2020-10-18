1b74c4bb1548e111cff940abf4202bf8dde353f9
BillNight <73046692+BillNight@users.noreply.github.com>
2020-10-18 05:20:38 +0200

    Update StaxToken.sol

--- contracts/StaxToken.sol
+++ contracts/StaxToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StaxToken is ERC20("StableX Token", "STAX"), Ownable {
+contract NextToken is ERC20("StableX Token", "Next"), Ownable {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
